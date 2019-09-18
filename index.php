a0ee65d36e79faafafd35e096ce1694088385f88
Dennis <dennisluijkenaar@gmail.com>
2019-09-18 13:14:11 +0200

    Added database connection

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
     <!-- Bootstrap CSS, then own CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+
+    <?php
+    // Including certain files
+    require_once("lib/db.php");
+    ?>
   </head>
   <body>
   <div class="container">
@@ -33,8 +38,6 @@
               </ul>
           </div>
       </nav>
-      <div class="divider">  
-      </div>
       <div id="carouselId" class="carousel slide" data-ride="carousel">
           <ol class="carousel-indicators">
               <li data-target="#carouselId" data-slide-to="0" class="active"></li>
