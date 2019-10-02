fb9cf19dec5e274629daed485e93f80289ba7a2c
321272 <321272@student.mboutrecht.nl>
2019-10-02 10:07:41 +0200

    Added contact.php, nav.php and some CSS

--- contact.php
+++ contact.php
@@ -21,11 +21,11 @@
               aria-expanded="false" aria-label="Toggle navigation"></button>
           <div class="collapse navbar-collapse" id="collapsibleNavId">
               <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                  <li class="nav-item active">
-                      <a class="nav-link" href="index.php">Webshop <span class="sr-only">(current)</span></a>
-                  </li>
                   <li class="nav-item">
-                      <a class="nav-link" href="contact.php">Contact</a>
+                      <a class="nav-link" href="index.php">Webshop</a>
+                  </li>
+                  <li class="nav-item active">
+                      <a class="nav-link" href="contact.php">Contact <span class="sr-only">(current)</span></a>
                   </li>
                   <li class="nav-item dropdown">
                       <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Inloggen</a>
@@ -36,35 +36,18 @@
                   </li>
               </ul>
           </div>
-      </nav>
+      </nav>    
+    
+    <div class="container rounded box-s" id="witcontainer">
+        <br>
+            <p><A HREF="mailto:321272@student.mboutrecht.nl?">mail@mail.com</A></p>
+            <p><a href="tel:555-555-1212">555-555-1212</a></p>
+            <p>Tekst Tekst Tekst</p>
+            <p>Tekst Tekst</p>
 
-
-      <div id="carouselId" class="carousel slide" data-ride="carousel">
-          <ol class="carousel-indicators">
-              <li data-target="#carouselId" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselId" data-slide-to="1"></li>
-              <li data-target="#carouselId" data-slide-to="2"></li>
-          </ol>
-          <div class="carousel-inner" role="listbox">
-              <div class="carousel-item active">
-                  <img class="lock img-fluid" src="./img/Scherm Ontwerp.jpg" alt="First slide">
-              </div>
-              <div class="carousel-item">
-                  <img class="lock img-fluid" src="./img/Scherm Ontwerp.jpg" alt="Second slide">
-              </div>
-              <div class="carousel-item">
-                  <img class="lock img-fluid" src="./img/Scherm Ontwerp.jpg" alt="Third slide">
-              </div>
-          </div>
-          <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselId" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-          </a>
-      </div>
+        <div class="gmap_canvas rounded">
+            <iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=voorstraat%2081%20asperen&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        </div>
     </div>
 
     
---
+++ nav.php
@@ -0,0 +1,25 @@
+  <div id="container">
+      <nav class="navbar navbar-expand-sm navbar-dark">
+          <a class="navbar-brand" href="#">
+              <img src="./img/JeanCenterInvertBlue.png" height="100" alt="logo">
+          </a>
+          <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+              aria-expanded="false" aria-label="Toggle navigation"></button>
+          <div class="collapse navbar-collapse" id="collapsibleNavId">
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                  <li class="nav-item active">
+                      <a class="nav-link" href="index.php">Webshop <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="contact.php">Contact</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Inloggen</a>
+                      <div class="dropdown-menu" aria-labelledby="dropdownId">
+                          <a class="dropdown-item" href="#">Inloggen</a>
+                          <a class="dropdown-item" href="#">Registreren</a>
+                      </div>
+                  </li>
+              </ul>
+          </div>
+      </nav>    
